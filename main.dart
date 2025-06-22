4b128b94ac9617f3d159668a5b07d1821149c0ef
Mwin456 <mwinplay03@gmail.com>
2025-06-22 10:13:18 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TalentYatraApp());
+}
+
+class TalentYatraApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TalentYatra',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: Text('TalentYatra')),
+        body: Center(child: Text('Welcome to TalentYatra!')),
+      ),
+    );
+  }
+}
